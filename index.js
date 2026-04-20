c2a7be41c2f830b4340a88955fc556d5367b5efb
madhumithaponnusamy <167957749+madhumithaponnusamy@users.noreply.github.com>
2026-04-20 12:25:54 +0530

    Initial NLP package

---
+++ index.js
@@ -0,0 +1,20 @@
+const { NlpManager } = require('node-nlp');
+const fs = require('fs');
+const path = require('path');
+
+class NlpService {
+    constructor() {
+        this.manager = new NlpManager({ languages: ['en'] });
+        this.modelPath = path.join(__dirname, 'model.nlp');
+    }
+
+    async process(text) {
+        if (fs.existsSync(this.modelPath)) {
+            const data = fs.readFileSync(this.modelPath, 'utf8');
+            this.manager.import(data);
+        }
+        return await this.manager.process('en', text);
+    }
+}
+
+module.exports = new NlpService();
